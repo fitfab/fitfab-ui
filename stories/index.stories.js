10f2519a7dac4373e50c653dfcfb588d148f7bc6
Miguel Julio <mjulio@artnet.com>
2018-01-10 17:51:32 -0500

    add eslintignore and update button to match the product pages

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,29 +4,19 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 //import { linkTo } from "@storybook/addon-links";
 
-//import { Button, Welcome } from '@storybook/react/demo';
 import { Button } from '../build/index'
 
-// storiesOf("Welcome", module).add("to Storybook", () => (
-//   <Welcome showApp={linkTo("Button")} />
-// ));
-
 storiesOf('Button', module)
-    .add('with text', () => (
-        <Button
-            bgColor="lightPink"
-            fontColor="white"
-            onClick={action('clicked')}
-        >
-            Hello Button
+    .add('Default', () => (
+        <Button onClick={action('clicked')}>get started</Button>
+    ))
+    .add('reverse', () => (
+        <Button reverse onClick={action('clicked')}>
+            get started
         </Button>
     ))
-    .add('with some emoji', () => (
-        <Button
-            bgColor="lightYellow"
-            fontColor="white"
-            onClick={action('clicked')}
-        >
-            😀 😎 👍 💯
+    .add('reverse outlined', () => (
+        <Button reverse outlined onClick={action('clicked')}>
+            get started
         </Button>
     ))
