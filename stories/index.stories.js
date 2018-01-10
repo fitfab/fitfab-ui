b0c1e2c32e43660050f5fd410ef56956fa29da70
Miguel Julio <mjulio@artnet.com>
2018-01-10 11:39:39 -0500

    add prettier and update rules to not semi

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,29 +1,32 @@
-import React from "react";
+import React from 'react'
 
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 //import { linkTo } from "@storybook/addon-links";
 
 //import { Button, Welcome } from '@storybook/react/demo';
-import { Button } from "../build/index";
+import { Button } from '../build/index'
 
 // storiesOf("Welcome", module).add("to Storybook", () => (
 //   <Welcome showApp={linkTo("Button")} />
 // ));
 
-storiesOf("Button", module)
-  .add("with text", () => (
-    <Button
-      bgColor="lightPink"
-      fontColor="white"
-      onClick={action("clicked")}
-      
-    >Hello Button</Button>
-  ))
-  .add("with some emoji", () => (
-    <Button
-      bgColor="lightYellow"
-      fontColor="white"
-      onClick={action("clicked")}
-    >😀 😎 👍 💯</Button>
-  ));
+storiesOf('Button', module)
+    .add('with text', () => (
+        <Button
+            bgColor="lightPink"
+            fontColor="white"
+            onClick={action('clicked')}
+        >
+            Hello Button
+        </Button>
+    ))
+    .add('with some emoji', () => (
+        <Button
+            bgColor="lightYellow"
+            fontColor="white"
+            onClick={action('clicked')}
+        >
+            😀 😎 👍 💯
+        </Button>
+    ))
