fb74b17c5508b4523d568b2249eec4e746709c7e
Miguel Julio <fitfab@yahoo.com>
2019-03-10 11:09:51 -0400

    add Fitfab logo and clean up story

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -7,5 +7,5 @@ import { linkTo } from '@storybook/addon-links'
 import { prettyDate, FitfabLogo } from '../lib/index'
 
 storiesOf('Fitfab UI', module)
-    .add('logo', () => <FitfabLogo />)
-    .add('prettyDate', () => <p>{prettyDate('08/02/1966')}</p>)
+    .add('logo', () => <FitfabLogo aria-label="Fitfab" />)
+    .add('prettyDate', () => <p>Happy birthday{prettyDate('08/02/1966')}</p>)
