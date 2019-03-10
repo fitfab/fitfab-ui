131e86bf117b0e5b69a44e7b808fd2fc922c1695
Miguel Julio <fitfab@yahoo.com>
2019-03-10 18:10:30 -0400

    add utils stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -7,19 +7,13 @@ import { withKnobs, text, boolean, number, object } from '@storybook/addon-knobs
 
 import { prettyDate, FitfabLogo } from '../lib/index'
 
-storiesOf('Fitfab UI', module)
-    .add('logo', () => {
-        return (
-            <FitfabLogo
-                aria-label="Fitfab"
-                brandColor={text('brandColor', '#FF4158')}
-                height={number('height', 43)}
-                width={number('width', 159)}
-            />
-        )
-    })
-    .add('prettyDate', () => (
-        <p>
-            {text('Label', 'Hello Storybook')} {prettyDate('08/02/1966')}
-        </p>
-    ))
+storiesOf('Fitfab UI', module).add('FitfabLogo', () => {
+    return (
+        <FitfabLogo
+            aria-label="Fitfab"
+            brandColor={text('brandColor', '#FF4158')}
+            height={number('height', 43)}
+            width={number('width', 159)}
+        />
+    )
+})
