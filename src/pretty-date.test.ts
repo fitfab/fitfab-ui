2a25c82e83d24929fa7ec7f99f4c526d6cdba364
Miguel Julio <fitfab@yahoo.com>
2019-03-11 15:20:51 -0400

    update test

--- src/pretty-date.test.ts
+++ src/pretty-date.test.ts
@@ -2,5 +2,5 @@ import { prettyDate } from './pretty-date'
 
 test('Display a pretty date', () => {
     const pretty = prettyDate('8/2/1966')
-    expect(pretty).toBe('August 2 1966')
+    expect(pretty).toBe('August 2, 1966')
 })
