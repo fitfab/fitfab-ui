5c80385aa1131f6e757f119b5f225dfad434e40b
Miguel Julio <fitfab@yahoo.com>
2019-06-11 17:29:00 -0400

    wip debugging test

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 // jest.config.js
 module.exports = {
+    roots: ['<rootDir>/src'],
     transform: {
         '^.+\\.(t|j)sx?$': 'ts-jest',
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    // Setup Enzyme
+    snapshotSerializers: ['enzyme-to-json/serializer'],
+    setupFilesAfterEnv: ['<rootDir>/src/setupEnzyme.ts'],
 }
--- src/icons/fitfablogo.test.tsx
+++ src/icons/fitfablogo.test.tsx
@@ -1,3 +1,8 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+import { FitfabLogo } from './FitfabLogo'
+
 test('this should test the custom hook', () => {
-    expect(false).toBe(false)
+    const logo = shallow(<FitfabLogo />)
+    expect(logo).toMatchSnapshot()
 })
