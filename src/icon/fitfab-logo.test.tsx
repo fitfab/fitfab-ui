e51aa7ba014481673fad244683a8310ab666b979
Miguel Julio <fitfab@yahoo.com>
2019-06-12 12:58:44 -0400

    update unit test for the logo

--- src/icon/fitfab-logo.test.tsx
+++ src/icon/fitfab-logo.test.tsx
@@ -3,8 +3,14 @@ import React from 'react'
 import { FitfabLogo } from './FitfabLogo'
 
 test('Render Fitfab logo with default props', () => {
-    const logo = shallow(<FitfabLogo />)
-
+    const wrapper = shallow(<FitfabLogo />)
     // Snapshot demo
-    expect(logo).toMatchSnapshot()
+    expect(wrapper).toMatchSnapshot()
+})
+
+test('Render Fitfab logo with custom props', () => {
+    const wrapper = shallow(<FitfabLogo width="150" brandColor="green" />)
+
+    expect(wrapper.props().brandColor).toEqual('green')
+    expect(wrapper.props().width).toEqual('150')
 })
