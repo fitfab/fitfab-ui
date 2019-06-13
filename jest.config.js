68921586a77230afe3200519a6cecc186b7b7d20
Miguel Julio <fitfab@yahoo.com>
2019-06-13 10:17:39 -0400

    add helpeful comments

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,46 @@
 // jest.config.js
 module.exports = {
+    /**
+     * Naming the test
+     */
+    displayName: 'Fitfab-UI',
+
+    /**
+     * Verbose console output per test
+     */
+    verbose: true,
+
+    /**
+     * The test environment that will be used for testing
+     */
+    testEnvironment: 'jsdom',
+
+    /**
+     * The transform config just tells jest
+     * to use ts-jest for ts / tsx files.
+     */
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
+
+    /**
+     * The testRegex tells Jest to look for tests in any __tests__ folder
+     * AND also any files anywhere that use the (.test|.spec).(ts|tsx)
+     */
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+
+    /**
+     * The moduleFileExtensions tells jest to recognize our file extensions.
+     * This is needed as we add ts/tsx into the defaults (js|jsx|json|node).
+     */
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    // Setup Enzyme
+    /**
+     * setup snapshot support with Enzyme
+     */
     snapshotSerializers: ['enzyme-to-json/serializer'],
+
+    /**
+     * Jest setup for Enzime
+     */
     setupFilesAfterEnv: ['<rootDir>/src/setup.enzyme.ts'],
 }
