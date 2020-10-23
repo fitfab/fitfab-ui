0b73bd728baee84e46a4d212be8190faca6e3a32
Miguel Julio <fitfab@yahoo.com>
2020-10-23 10:29:43 -0400

    update css

--- src/typography/index.tsx
+++ src/typography/index.tsx
@@ -6,33 +6,33 @@ const H1View = styled.h1`
     -webkit-font-smoothing: subpixel-antialiased;
     color: #333;
     font: normal normal 400 32px/40px Inter, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans,
-        sans-serif, BlinkMacSystemFont, Helvetica Neue, Arial, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol,
-        Noto Color Emoji;
+        sans-serif, BlinkMacSystemFont, Helvetica Neue, Arial;
     margin: 0 0 16px 0;
+    letter-spacing: 2px;
 `
 
 const H2View = styled.h2`
     color: #333;
     font: normal normal 400 28px/36px Inter, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans,
-        sans-serif, BlinkMacSystemFont, Helvetica Neue, Arial, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol,
-        Noto Color Emoji;
+        sans-serif, BlinkMacSystemFont, Helvetica Neue, Arial;
     margin: 0 0 16px 0;
+    letter-spacing: 2px;
 `
 
 const H3View = styled.h3`
     color: #333;
     font: normal normal 500 20px/24px Inter, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans,
-        sans-serif, BlinkMacSystemFont, Helvetica Neue, Arial, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol,
-        Noto Color Emoji;
+        sans-serif, BlinkMacSystemFont, Helvetica Neue, Arial;
     margin: 0 0 16px 0;
+    letter-spacing: 2px;
 `
 
 const H4View = styled.h4`
     color: #333;
     font: normal normal 600 16px/20px Inter, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans,
-        sans-serif, BlinkMacSystemFont, Helvetica Neue, Arial, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol,
-        Noto Color Emoji;
+        sans-serif, BlinkMacSystemFont, Helvetica Neue, Arial;
     margin: 0 0 8px 0;
+    letter-spacing: 1px;
 `
 
 export interface HeadingProps {
@@ -41,19 +41,19 @@ export interface HeadingProps {
 }
 
 export const Heading: React.FC<HeadingProps> = props => {
-    const { variant, children } = props
-
+    const { children } = props
+    const variant = props.variant.toLowerCase()
     if (variant !== '') {
-        if (variant === 'H1') {
+        if (variant === 'h1') {
             return <H1View>{children}</H1View>
         }
-        if (variant === 'H2') {
+        if (variant === 'h2') {
             return <H2View>{children}</H2View>
         }
-        if (variant === 'H3') {
+        if (variant === 'h3') {
             return <H3View>{children}</H3View>
         }
-        if (variant === 'H4') {
+        if (variant === 'h4') {
             return <H4View>{children}</H4View>
         }
     }
@@ -64,13 +64,15 @@ export const Heading: React.FC<HeadingProps> = props => {
     )
 }
 
-export interface CopyProps {
-    size: string
+export interface TextProps {
+    size: number
 }
-export const Copy = styled.p<CopyProps>`
-    color: #404040;
+export const Text = styled.p<TextProps>`
+    color: #6f6f6f;
     font-family: Inter, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif,
-        BlinkMacSystemFont, Helvetica Neue, Arial, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
-    font-size: ${p => (p.size ? p.size : '16px')};
+        BlinkMacSystemFont, Helvetica Neue, Arial;
+    font-size: ${p => (p.size ? p.size : '16')}px;
+    letter-spacing: 1px;
+    line-height: ${p => (p.size ? p.size + p.size / 2 : '24')}px;
     margin: 0 0 8px 0;
 `
