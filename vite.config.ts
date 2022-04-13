1995a54fd3e17b86544e3ddfbddd88205e07e547
Miguel Julio <fitfab@yahoo.com>
2022-04-12 21:49:57 -0400

    chore: add vitejs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
+import path from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import pkg from './package.json'
+import dts from 'vite-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            formats: ['es'],
+            name: pkg.name,
+            fileName: format => `${pkg.name}.${format}.js`,
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                },
+            },
+        },
+    },
+    plugins: [dts()],
 })
